d8daabd4430efdc64ff9700add6274fbbd987d6a
Alexseusik <alexgnibeda03@gmail.com>
2023-07-01 14:37:02 +0300

    task 2

---
+++ task2.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
